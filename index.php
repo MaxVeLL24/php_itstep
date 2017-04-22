fca3036a8fba630e6d4a828e505f05591a3f4066
MaxVeLL24 <pawlenko1993@ukr.net>
2017-04-22 21:00:05 +0300

    PHP_start

---
+++ index.php
@@ -0,0 +1,17 @@
+<html<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<?php
+
+
+for ($i = 30; $i <= 35; $i++) {
+    echo $i * $i;
+    echo '<br>';
+}
+
+?>
+</body>
+</html>
