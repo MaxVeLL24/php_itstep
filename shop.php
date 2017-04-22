3d54f9107e26bcf9ceee50750050f8015f34f8fa
MaxVeLL24 <pawlenko1993@ukr.net>
2017-04-22 21:03:13 +0300

    second revision

--- shop.php
+++ shop.php
@@ -8,33 +8,6 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<?php
-
-//var_dump($menu);
-
-//
-//$a = 2;
-//$b = 3;
-//$i = 1;
-//$result = 1;
-//while ($i <= $b) {
-//    $result *= $a;
-//    $i++;
-//}
-//var_dump($result);
-//echo '<br>';
-//$k = 3;
-//$l = 2;
-//$resultFor = 1;
-//for ($j = 1; $j <= $l; $j++):
-//    $resultFor *= $k;
-//endfor;
-//var_dump($resultFor);
-
-//$arr = ['HTML', 'JS', 'PHP'];
-//var_dump($arr)
-?>
-
 <nav>
     <ul class="menu">
         <?php
